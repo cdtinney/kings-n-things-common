fde66cc0054f176f84e0f0de7824b0a89c880780
cdtinney <colintinney@cmail.carleton.ca>
2014-03-29 18:21:04 -0400

    add trace log level

--- kings-n-things-common/src/com/kingsandthings/logging/LogLevel.java
+++ kings-n-things-common/src/com/kingsandthings/logging/LogLevel.java
@@ -7,6 +7,7 @@ public class LogLevel extends Level {
 	
 	public static final Level STATUS = new LogLevel("STATUS", Level.INFO.intValue() + 1);
 	public static final Level DEBUG = new LogLevel("DEBUG", Level.INFO.intValue() - 1);
+	public static final Level TRACE = new LogLevel("TRACE", Level.INFO.intValue() - 2);
 
 	public LogLevel(String name, int value) {
 		super(name, value);
